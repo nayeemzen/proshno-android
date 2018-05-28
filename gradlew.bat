065031fcee24c0ed4fdae57594b05a55eb8732b1
Nayeem Zen <nayeem.zen@mail.utoronto.ca>
2018-05-28 03:25:29 -0400

    Revert gradlew changes from IntelliJ refactor

--- gradlew.bat
+++ gradlew.bat
@@ -57,7 +57,7 @@ set CMD_LINE_ARGS=
 set _SKIP=2
 
 :win9xME_args_slurp
-if "gameContainerBottomPadding%~1" == "gameContainerBottomPadding" goto execute
+if "x%~1" == "x" goto execute
 
 set CMD_LINE_ARGS=%*
 goto execute
